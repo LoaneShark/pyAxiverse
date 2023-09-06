Fixed bugs in code, more verbose printout and config options, more robust unit scaling and dimensionality handling

piaxi_numerics.py
@@ -15,33 +15,19 @@ def set_params(params_in: dict, sample_delta=True, sample_theta=True, t_max=10, 
                k_max=200, k_min=1, k_N=200):
     # Define global variables
     global params
-    global t
-    global t_span
-    global t_num
-    global t_step
-    global k_values
-    global k_span
-    global k_num
-    global k_step
+    global t, t_span, t_num, t_step
+    global k_values, k_span, k_num, k_step
     global e
-    global F
-    global p_t
     global eps
-    global L3
-    global L4
-    global l1
-    global l2
-    global l3
-    global l4
-    global A_0
-    global Adot_0
-    global A_pm
-    global m
-    global m_0
-    global p
-    global amps
-    global d
-    global Th
+    global F
+    global c, h, G
+    global L3, L4
+    global l1, l2, l3, l4
+    global A_0, Adot_0, A_pm
+    global m, m_0, m_q
+    global p, p_t, p_0, amps
+    global d, Th
+    global qm, qc, dqm, eps_c, xi
     
     # t domain
     t_span = [t_min, t_max]  # Time range
@@ -56,14 +42,19 @@ def set_params(params_in: dict, sample_delta=True, sample_theta=True, t_max=10, 
     res_con = params_in['res_con']
 
     # Set constants of model (otherwise default)
-    e = params_in['e'] if 'e' in params_in else 0.3        #
-    F = params_in['F'] if 'F' in params_in else 1e20       # eV
+    e = params_in['e']     if 'e' in params_in else 0.3        #
+    F = params_in['F']     if 'F' in params_in else 1e20       # eV
     p_t = params_in['p_t'] if 'p_t' in params_in else 0.4  # eV / cm^3
+    
+    # Fundamental constants
+    c = params_in['c']
+    h = params_in['h']
+    G = params_in['G']
     
     ## Tuneable constants
     # millicharge, vary to enable/disable charged species (10e-15 = OFF, 10e-25 = ON)
     #eps  = 1e-25   # (unitless)
-    eps  = params_in['eps'] #
+    eps  = params_in['eps']
     
     # dQCD sector parameters
     qm    = params_in['qm']    if 'qm'    in params_in else np.full((3, ), None)
@@ -88,19 +79,21 @@ def set_params(params_in: dict, sample_delta=True, sample_theta=True, t_max=10, 
 
     # masses for real, complex, and charged species in range (10e-8, 10e-4) [eV]
     m   = params_in['m']
-    m_0 = params_in['m_0'] if 'm_0' in params_in else min(m[0])           # unit mass value
+    m_q = params_in['m_scale']                                            # dark quark mass scale
+    m_0 = params_in['m_0']      if 'm_0' in params_in else min(m[0])      # unit mass value
     N_r = len(params_in['m_r']) if 'm_r' in params_in else 1              # number of real species
-    m_r = params_in['m_r'] if 'm_r' in params_in else np.full((N_r, ), m[0]) # (neutral) real species
+    m_r = params_in['m_r']      if 'm_r' in params_in else np.full((N_r, ), m[0]) # (neutral) real species
     N_n = len(params_in['m_n']) if 'm_n' in params_in else 1              # number of neutral species
-    m_n = params_in['m_n'] if 'm_n' in params_in else np.full((N_n, ), m[1]) # neutral (complex) species
+    m_n = params_in['m_n']      if 'm_n' in params_in else np.full((N_n, ), m[1]) # neutral (complex) species
     N_c = len(params_in['m_c']) if 'm_c' in params_in else 1              # number of charged species
-    m_c = params_in['m_c'] if 'm_c' in params_in else np.full((N_c, ), m[2]) # charged (complex) species
+    m_c = params_in['m_c']      if 'm_c' in params_in else np.full((N_c, ), m[2]) # charged (complex) species
 
     # local DM densities for each species [eV/cm^3]
-    p    = params_in['p'] if 'p' in params_in else np.array([np.full(p_t/3,N_r), np.full(p_t/3,N_n), np.full(p_t/3,N_c)], dtype=object) # default to equal distribution
+    p    = params_in['p']   if 'p'   in params_in else np.array([np.full(p_t/3,N_r), np.full(p_t/3,N_n), np.full(p_t/3,N_c)], dtype=object) # default to equal distribution
     p_r  = params_in['p_r'] if 'p_r' in params_in else np.full((N_r, ), None)   # (neutral) real species
     p_n  = params_in['p_n'] if 'p_n' in params_in else np.full((N_n, ), None)   # neutral (complex) species
     p_c  = params_in['p_c'] if 'p_c' in params_in else np.full((N_c, ), None)   # charged (complex) species
+    p_0  = params_in['p_unit'] if 'p_unit' in params_in else 1.
     # initial amplitudes for each species
     amps = params_in['amps'] if 'amps' in params_in else [np.sqrt(2 * p[i]) / m[i] for i in range(len(m))] # default to equal distribution
 
@@ -111,22 +104,33 @@ def set_params(params_in: dict, sample_delta=True, sample_theta=True, t_max=10, 
     Th   = params_in['Th'] if 'Th' in params_in else np.array([np.zeros(float(N_r)), np.zeros(float(N_n)), np.zeros(float(N_c))], dtype=object)
     
     # Sample phases from normal distribution, sampled within range (0, 2pi)
-    mu_d   = params_in['mu_d'] if 'mu_d' in params_in else np.pi           # local phase mean
-    sig_d  = params_in['sig_d'] if 'sig_d' in params_in else np.pi / 3     # local phase standard deviation
-    mu_Th  = params_in['mu_Th'] if 'mu_Th' in params_in else np.pi        # global phase mean
+    mu_d   = params_in['mu_d']   if 'mu_d' in params_in else np.pi           # local phase mean
+    sig_d  = params_in['sig_d']  if 'sig_d' in params_in else np.pi / 3     # local phase standard deviation
+    mu_Th  = params_in['mu_Th']  if 'mu_Th' in params_in else np.pi        # global phase mean
     sig_Th = params_in['sig_Th'] if 'sig_Th' in params_in else np.pi / 3  # global phase standard deviation
     if sample_delta and 'd' not in params_in:
             d = [np.mod(np.random.normal(mu_d, sig_d, len(d_i)), 2*np.pi) for d_i in d]
     if sample_theta and 'Th' not in params_in:
             Th = [np.mod(np.random.normal(mu_Th, sig_Th, len(Th_i)), 2*np.pi) for Th_i in Th]
             
+    # rescaling and unit configuration
+    unitful_m      = params_in['unitful_m']
+    rescale_m      = params_in['rescale_m']
+    unitful_k      = params_in['unitful_k']
+    rescale_k      = params_in['rescale_k']
+    unitful_amps   = params_in['unitful_amps']
+    rescale_amps   = params_in['rescale_amps']
+    rescale_consts = params_in['rescale_consts']
+            
     # Store for local use, and then return
     params = {'e': e, 'F': F, 'p_t': p_t, 'eps': eps, 'L3': L3, 'L4': L4, 'l1': l1, 'l2': l2, 'l3': l3, 'l4': l4,
-              'A_0': A_0, 'Adot_0': Adot_0, 'A_pm': A_pm, 'amps': amps, 'd': d, 'Th': Th,
-              'qm': qm, 'qc': qc, 'dqm': dqm, 'eps_c': eps_c, 'xi': xi, 'N_r': N_r, 'N_n': N_n, 'N_c': N_c,
+              'A_0': A_0, 'Adot_0': Adot_0, 'A_pm': A_pm, 'amps': amps, 'd': d, 'Th': Th, 'h': h, 'c': c, 'G': G,
+              'qm': qm, 'qc': qc, 'dqm': dqm, 'eps_c': eps_c, 'xi': xi, 'N_r': N_r, 'N_n': N_n, 'N_c': N_c, 'p_0': p_0,
               'm': m, 'm_r': m_r, 'm_n': m_n, 'm_c': m_c, 'p': p, 'p_r': p_r, 'p_n': p_n, 'p_c': p_c, 'm_0': m_0,
               'mu_d': mu_d, 'sig_d': sig_d, 'mu_Th': mu_Th, 'sig_Th': sig_Th, 'k_span': k_span, 'k_num': k_num,
-              't_span': t_span, 't_num': t_num, 'A_sens': A_sens, 't_sens': t_sens, 'res_con': res_con}
+              't_span': t_span, 't_num': t_num, 'A_sens': A_sens, 't_sens': t_sens, 'res_con': res_con, 
+              'unitful_m': unitful_m, 'rescale_m': rescale_m, 'unitful_amps': unitful_amps, 'rescale_amps': rescale_amps, 
+              'rescale_k': rescale_k, 'rescale_consts': rescale_consts}
     
     return params
 
@@ -180,35 +184,45 @@ def solve_subsystem(system_in, params, k):
     return y
     
 # Helper function to print parameters of model
-def get_text_params(case='full'):
+def get_text_params(case='full', units_in={}):
+    units = {}
+    for key in ['c', 'h', 'G', 'm', 'k', 'p', 'amp', 'Lambda', 'lambda', 'F', 't', 'Theta', 'delta', 'eps']:
+        if key not in units_in or units_in[key] == 1:
+            units[key] = ''
+        elif key in ['Lambda','F'] and units_in[key] == 'm_u':
+            units[key] = '\quad[%s]' % ('eV')
+        else:
+            units[key] = '\quad[%s]' % (units_in[key])
+    
     if case == 'simple':
         textstr1 = '\n'.join((
                 r'$A_\pm(t = 0)=%.2f$' % (A_0, ),
                 r'$\dot{A}_\pm (t = 0)=%.2f$' % (Adot_0, ),
                 r'$\pm=%s$' % (signstr[A_pm], ),
                 '\n',
-                r'$\varepsilon=%.0e$' % (eps, ),
+                r'$\varepsilon=%.0e%s$' % (eps, ),
+                r'$F_{\pi}=%.0e%s$' % (F, units['F']),
                 '\n',
-                r'$\lambda_1=%d$' % (l1, ),
-                r'$\lambda_2=%d$' % (l2, ),
-                r'$\lambda_3=%d$' % (l3, ),
-                r'$\lambda_4=%d$' % (l4, ),
-                r'$\Lambda_3=%.0e$' % (L3, ),
-                r'$\Lambda_4=%.0e$' % (L4, ),
+                r'$\lambda_1=%d%s$' % (l1, units['lambda']),
+                r'$\lambda_2=%d%s$' % (l2, units['lambda']),
+                r'$\lambda_3=%d%s$' % (l3, units['lambda']),
+                r'$\lambda_4=%d%s$' % (l4, units['lambda']),
+                r'$\Lambda_3=%.0e%s$' % (L3, units['Lambda']),
+                r'$\Lambda_4=%.0e%s$' % (L4, units['Lambda']),
                 '\n',
-                r'$\Delta k=%.2f$' % (k_step, ),
-                r'$k \in \left[%d, %d\right]$' % (k_span[0], k_span[1]),
+                r'$\Delta k=%.2f%s$' % (k_step, units['k']),
+                r'$k \in \left[%d, %d\\right]$' % (k_span[0], k_span[1]),
 
         ))
 
         textstr2 = '\n'.join((
-                r'$m_{(0)}=%.0e eV$' % (m[0], ),
-                r'$m_{(\pi)}=%.0e eV$' % (m[1], ),
-                r'$m_{(\pm)}=%.0e eV$' % (m[2], ),
+                r'$m_{(0)}=%.0e%s$' % (m[0], units['m']),
+                r'$m_{(\pi)}=%.0e%s$' % (m[1], units['m']),
+                r'$m_{(\pm)}=%.0e%s$' % (m[2], units['m']),
                 '\n',
-                r'$\pi_{(0)}=%.2f$' % (amps[0], ),
-                r'$\pi_{(\pi)}=%.2f$' % (amps[1], ),
-                r'$\pi_{(\pm)}=%.2f$' % (amps[2], ),
+                r'$\pi_{(0)}=%.2f%s$' % (amps[0], units['amp']),
+                r'$\pi_{(\pi)}=%.2f%s$' % (amps[1], units['amp']),
+                r'$\pi_{(\pm)}=%.2f%s$' % (amps[2], units['amp']),
                 '\n',
                 r'$\delta_{(0)}=%.2f \pi$' % (d[0]/np.pi, ),
                 r'$\delta_{(\pi)}=%.2f \pi$' % (d[1]/np.pi, ),
@@ -216,8 +230,8 @@ def get_text_params(case='full'):
                 '\n',
                 r'$\Theta_{(\pi)}=%.2f \pi$' % (Th[1]/np.pi, ),
                 '\n',
-                r'$\Delta t=%f$' % (t_step, ),
-                r'$t \in \left[%d, %d\right]$' % (t_span[0], t_span[1]),
+                r'$\Delta t=%f%s$' % (t_step, units['t']),
+                r'$t \in \left[%d, %d\\right]$' % (t_span[0], t_span[1]),
         ))
     else:
         textstr1 = '\n'.join((
@@ -226,33 +240,43 @@ def get_text_params(case='full'):
                 r'$\pm=%s$' % (signstr[A_pm], ),
                 '\n',
                 r'$\varepsilon=%.0e$' % (eps, ),
+                r'$F_{\pi}=%.0e%s$' % (F, units['F']),
+                r'$c = h = G = 1$' if all([units[key] == '' for key in ['c', 'h', 'G']]) else '\n'.join([r'$%s=%.2e%s$' % (key, val, units[key]) for key, val in zip(['c', 'h', 'G'], [c, h, G])]),
                 '\n',
-                r'$\lambda_1=%d$' % (l1, ),
-                r'$\lambda_2=%d$' % (l2, ),
-                r'$\lambda_3=%d$' % (l3, ),
-                r'$\lambda_4=%d$' % (l4, ),
-                r'$\Lambda_3=%.0e$' % (L3, ),
-                r'$\Lambda_4=%.0e$' % (L4, ),
+                r'$\lambda_1=%d%s$' % (l1, units['lambda']),
+                r'$\lambda_2=%d%s$' % (l2, units['lambda']),
+                r'$\lambda_3=%d%s$' % (l3, units['lambda']),
+                r'$\lambda_4=%d%s$' % (l4, units['lambda']),
+                r'$\Lambda_3=%.0e%s$' % (L3, units['Lambda']),
+                r'$\Lambda_4=%.0e%s$' % (L4, units['Lambda']),
                 '\n',
-                r'$\Delta k=%.2f$' % (k_step, ),
-                r'$k \in \left[%d, %d\right]$' % (k_span[0], k_span[1]),
+                r'$\Delta k=%.2f%s$' % (k_step, units['k']),
+                r'$k \in [%d, %d]$' % (k_span[0], k_span[1]),
                 '\n',
-                r'$\Delta t=%f$' % (t_step, ),
-                r'$t \in \left[%d, %d\right]$' % (t_span[0], t_span[1]),
+                r'$\Delta t=%f%s$' % (t_step, units['t']),
+                r'$t \in [%d, %d]$' % (t_span[0], t_span[1]),
 
         ))
         m0_mask = np.ma.getmask(m[0]) if np.ma.getmask(m[0]) else np.full_like(m[0], False)
         m1_mask = np.ma.getmask(m[1]) if np.ma.getmask(m[1]) else np.full_like(m[1], False)
         m2_mask = np.ma.getmask(m[2]) if np.ma.getmask(m[2]) else np.full_like(m[2], False)
         textstr2 = '\n'.join((
-                r'$m_{I} = %.2e$' % (m_0, ),
+                r'$m_{\{I\}} = [%s]\quad%.0e eV$' % (', '.join('%d' % q for q in qm), m_q),
+                '' if units_in['m'] == 'eV' else r'$m_{u} = %.2e\quad[eV]$' % (m_0, ),
+                r'$m%s$' % units['m'],
                 ' '.join([r'$m_{(0),%d}=%.2e$'   % (i+1, m[0][i], ) for i in range(len(m[0])) if not m0_mask[i]]),
                 ' '.join([r'$m_{(\pi),%d}=%.2e$' % (i+1, m[1][i], ) for i in range(len(m[1])) if not m1_mask[i]]),
                 ' '.join([r'$m_{(\pm),%d}=%.2e$' % (i+1, m[2][i], ) for i in range(len(m[2])) if not m2_mask[i]]),
                 '\n',
+                r'$\pi%s$' % units['amp'],
                 ' '.join([r'$\pi_{(0),%d}=%.2e$'   % (i+1, amps[0][i], ) for i in range(len(amps[0])) if not m0_mask[i]]),
                 ' '.join([r'$\pi_{(\pi),%d}=%.2e$' % (i+1, amps[1][i], ) for i in range(len(amps[1])) if not m1_mask[i]]),
                 ' '.join([r'$\pi_{(\pm),%d}=%.2e$' % (i+1, amps[2][i], ) for i in range(len(amps[2])) if not m2_mask[i]]),
+                '\n',
+                r'$\rho\quad[eV/cm^3]$',
+                ' '.join([r'$\rho_{(0),%d}=%.2e$'   % (i+1, p[0][i]/p_0, ) for i in range(len(p[0])) if not m0_mask[i]]),
+                ' '.join([r'$\rho_{(\pi),%d}=%.2e$' % (i+1, p[1][i]/p_0, ) for i in range(len(p[1])) if not m1_mask[i]]),
+                ' '.join([r'$\rho_{(\pm),%d}=%.2e$' % (i+1, p[2][i]/p_0, ) for i in range(len(p[2])) if not m2_mask[i]]),
                 '\n',
                 ' '.join([r'$\delta_{(0),%d}=%.2f \pi$'   % (i+1, d[0][i]/np.pi, ) for i in range(len(d[0])) if not m0_mask[i]]),
                 ' '.join([r'$\delta_{(\pi),%d}=%.2f \pi$' % (i+1, d[1][i]/np.pi, ) for i in range(len(d[1])) if not m1_mask[i]]),
@@ -263,4 +287,4 @@ def get_text_params(case='full'):
                 ' '.join([r'$\Theta_{(\pm),%d}=%.2f \pi$' % (i+1, Th[2][i]/np.pi, ) for i in range(len(Th[2])) if not m2_mask[i]]),
         ))
         
-    return textstr1, textstr2, 
+    return textstr1, textstr2