bff19c08a6ae267653d990e8a0e65ebb498dc002
Santiago Loane <santiagoloane@brown.edu>
2024-02-20 16:26:48 -0500

    updated runscripts for versatility

--- scripts/pi_axiverse_star.sh
+++ scripts/pi_axiverse_star.sh
@@ -43,22 +43,28 @@ PIAXI_COREMEM=$SLURM_MEM_PER_NODE
 
 PIAXI_DENSITY="1e22"
 PIAXI_VERBOSITY=5
-PIAXI_N_SAMPLES=5
+PIAXI_N_SAMPLES=3
 
 PIAXI_N_TIMES=300
 PIAXI_MAX_TIME=30
 PIAXI_MAX_KMODE=200
 PIAXI_KMODE_RES=0.1
 
-PIAXI_N_QMASS=20
+PIAXI_N_QMASS=10
 PIAXI_MASS_RANGE="-60 -80"
 
 #PIAXI_L_RANGE="0 30"
-#PIAXI_L3_RANGE="0 30"
-PIAXI_L4_RANGE="0 30"
 #PIAXI_N_L=30
-#PIAXI_N_L3=30
-PIAXI_N_L4=30
+if [[ "$INPUT_ARG1" = "FULL" ]]
+then
+    PIAXI_L3_RANGE="0 30"
+    PIAXI_N_L3=5
+else
+    PIAXI_L3_RANGE="None"
+    PIAXI_N_L3="None"
+fi
+PIAXI_L4_RANGE="0 30"
+PIAXI_N_L4=5
 
 #PIAXI_F="1e9"
 PIAXI_F_RANGE="5 10"
@@ -67,4 +73,4 @@ PIAXI_N_F=6
 PIAXI_N_EPS=5
 PIAXI_EPS_RANGE="-25 -5"
 
-python piaxiverse.py --use_natural_units --use_mass_units --num_cores $PIAXI_N_CORES --mem_per_core $PIAXI_COREMEM --num_samples $PIAXI_N_SAMPLES --t $PIAXI_MAX_TIME --tN $PIAXI_N_TIMES --use_mass_units $PIAXI_MASS_UNIT --verbosity $PIAXI_VERBOSITY --k $PIAXI_MAX_KMODE --k_res $PIAXI_KMODE_RES --scan_mass $PIAXI_MASS_RANGE --scan_mass_N $PIAXI_N_QMASS --scan_Lambda4 $PIAXI_L4_RANGE --scan_Lambda4_N $PIAXI_N_L4 --config_name $PIAXI_SYS_NAME --rho $PIAXI_DENSITY --dqm_c $PIAXI_DQMC  --scan_F $PIAXI_F_RANGE --scan_F_N $PIAXI_N_F --scan_epsilon $PIAXI_EPS_RANGE --scan_epsilon_N $PIAXI_N_EPS --save_output_files --make_plots
+python piaxiverse.py --use_natural_units --use_mass_units --num_cores $PIAXI_N_CORES --mem_per_core $PIAXI_COREMEM --num_samples $PIAXI_N_SAMPLES --t $PIAXI_MAX_TIME --tN $PIAXI_N_TIMES --use_mass_units $PIAXI_MASS_UNIT --verbosity $PIAXI_VERBOSITY --k $PIAXI_MAX_KMODE --k_res $PIAXI_KMODE_RES --scan_mass $PIAXI_MASS_RANGE --scan_mass_N $PIAXI_N_QMASS --scan_Lambda4 $PIAXI_L4_RANGE --scan_Lambda4_N $PIAXI_N_L4 --scan_Lambda3 $PIAXI_L3_RANGE --scan_Lambda3_N $PIAXI_N_L3 --config_name $PIAXI_SYS_NAME --rho $PIAXI_DENSITY --dqm_c $PIAXI_DQMC  --scan_F $PIAXI_F_RANGE --scan_F_N $PIAXI_N_F --scan_epsilon $PIAXI_EPS_RANGE --scan_epsilon_N $PIAXI_N_EPS --save_output_files --make_plots
