5efea97cddbe77fc2e171acb647a0d28fd9836a2
Santiago Loane <santiagoloane@brown.edu>
2024-06-13 14:21:56 -0400

    Updated runscripts

--- scripts/sample_pi_axiverse.sh
+++ scripts/sample_pi_axiverse.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -n 100
 #SBATCH -N 1
-#SBATCH --time 2:00:00
-#SBATCH --mem 300G
+#SBATCH --time 48:00:00
+#SBATCH --mem 200G
 #SBATCH --job-name pi_axiverse_array
 #SBATCH --output ./logs/pi_axiverse_log-%J.txt
 #SBATCH -p batch
@@ -16,7 +16,9 @@ source /oscar/runtime/software/external/miniconda3/23.11.0/etc/profile.d/conda.s
 conda activate piaxiverse
 
 INPUT_ARGFILE="${1}"
-INPUT_VERBOSITY="${2:-1}"
+INPUT_STARTLINE="${2:1}"
+INPUT_ENDLINE="${3:100}"
+INPUT_VERBOSITY="${4:-1}"
 
 # Verbose printouts for debugging conda environment
 PIAXI_VERBOSITY="${INPUT_VERBOSITY}"
@@ -33,7 +35,10 @@ PIAXI_JOB_QOS="${SLURM_JOB_QOS}"
 
 PIAXI_SLURM_ARGS="--num_cores ${PIAXI_N_CORES} --num_nodes ${PIAXI_N_NODES} --job_qos ${PIAXI_JOB_QOS} --mem_per_core ${PIAXI_COREMEM}"
 
-PIAXI_COMMAND=$(sed -n "${SLURM_ARRAY_TASK_ID}p" < ${INPUT_ARGFILE})
-echo "${PIAXI_COMMAND}"
+for i in ${"${INPUT_STARTLINE}"..."${INPUT_ENDLINE}"}
+do
+    PIAXI_COMMAND=$(sed -n "${SLURM_ARRAY_TASK_ID}p" < ${INPUT_ARGFILE})
+    echo "LINE ${i}: ${PIAXI_COMMAND}"
 
-eval "${PIAXI_COMMAND} ${PIAXI_SLURM_ARGS}"
+    eval "${PIAXI_COMMAND} ${PIAXI_SLURM_ARGS}"
+done
